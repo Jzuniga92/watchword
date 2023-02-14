89978cdf97d86ea74a8386b76c9ab1791b207ed3
Joselyn Zuniga <joselynzuniga@joselyns-mbp.attlocal.net>
2023-02-13 18:32:59 -0600

    updated scripts.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,64 @@
+var characterLength = 12;
+var choiceArr = [];
+
+var specialCharArr = ["!","@","#","$","%","^","&","*","?"];
+var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numberArr = ["1","2","3","4","5","6","7","8","9","0",];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-
-
-  alert("Select ok to include uppercase")
-
-  alert("Select ok to include lowercase")
-
-  alert("Select ok to include numbers")
-
-  alert("Select ok to include special characters")
-  var numbercharacters = window.prompt ("Chose a number between 8 and 128")
-
-//1. prompt the user for the password criteria
-// a. password length has to be 8 - 128
-// b. lowercase, uppercase, number, and special character
-// 2. validate the input
-// 3. generate the password
-
-
-//4. dispaplay the password
-  return "password will go here";
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts(); 
   var passwordText = document.querySelector("#password");
+  
+  if (correctPrompts) {
+   var newPassword = generatePassword();
+   passwordText.value = newPassword;
+  }
 
-  passwordText.value = password;
+function generatePassword() {
 
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = (Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password; 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getPrompts(){
+characterLength = parseInt(prompt("Chose a length for your password 8 - 128 characters"));
+
+if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length has to be a number, 8 - 128. Try Again!");
+    return false;
+  } 
+}
+
+if (confirm("Select ok to include uppercase")) {
+  choiceArr = choiceArr.concat(upperCaseArr);
+}
+
+if (confirm("Select ok to include lowercase")) {
+  choiceArr = choiceArr.concat(lowerCaseArr);
+}
+
+if (confirm("Select ok to include numbers")) {
+  choiceArr = choiceArr.concat(numberArr);
+}
+
+if (confirm("Select ok to include special characters")) {
+  choiceArr = choiceArr.concat(specialCharArr);
+}
+return true;
+// 3. generate the password
+}
+
+//4. dispaplay the password
+  //return "password goes here";
